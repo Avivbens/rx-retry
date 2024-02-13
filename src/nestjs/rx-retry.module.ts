cd9380783fcdd0e50920f9eb7b1935546f66000d
Aviv Ben Shahar <avivbens87@gmail.com>
2024-02-13 16:48:12 +0200

    feat: support `isGlobal` property for global app access - configure once

--- src/nestjs/rx-retry.module.ts
+++ src/nestjs/rx-retry.module.ts
@@ -1,4 +1,7 @@
+import type { DynamicModule } from '@nestjs/common'
 import { Module } from '@nestjs/common'
+import type { ModuleConfiguration } from '../models/retry-config.model'
+import type { ASYNC_OPTIONS_TYPE, OPTIONS_TYPE } from './rx-retry.module-builder'
 import { ConfigurableModuleClass } from './rx-retry.module-builder'
 import { RxRetryService } from './rx-retry.service'
 
@@ -6,4 +9,34 @@ import { RxRetryService } from './rx-retry.service'
     providers: [RxRetryService],
     exports: [RxRetryService],
 })
-export class RxRetryModule extends ConfigurableModuleClass {}
+export class RxRetryModule extends ConfigurableModuleClass {
+    /**
+     * Configure the `rx-retry` module statically.
+     *
+     * @param options options to configure the cache manager
+     */
+    public static register(options: typeof OPTIONS_TYPE): DynamicModule {
+        const moduleDefinition = super.register(options)
+
+        return {
+            ...moduleDefinition,
+            global: options.isGlobal ?? false,
+        }
+    }
+
+    /**
+     * Configure the `rx-retry` module dynamically.
+     *
+     * @param options method for dynamically supplying cache manager configuration options
+     */
+    public static registerAsync(
+        options: typeof ASYNC_OPTIONS_TYPE & Pick<ModuleConfiguration, 'isGlobal'>,
+    ): DynamicModule {
+        const moduleDefinition = super.registerAsync(options)
+
+        return {
+            ...moduleDefinition,
+            global: options.isGlobal ?? false,
+        }
+    }
+}
